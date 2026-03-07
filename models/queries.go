fd4bf7ed4fc3c28b8c660892c39c3b4563126cd8
Ashutosh Gupta <ashutoshgupta312@gmail.com>
2026-03-07 23:51:02 +0530

    feat: add queries to get and create users

--- models/queries.go
+++ models/queries.go
@@ -3,7 +3,15 @@ package models
 import "github.com/jmoiron/sqlx"
 
 type Queries struct {
+	// User queries
+	GetUserById   *sqlx.Stmt `query:"get-user-by-id"`
+	GetUserByUUID *sqlx.Stmt `query:"get-user-by-uuid"`
+	CreateUser    *sqlx.Stmt `query:"create-user"`
+
+	// Exercise queries
 	GetExercises    *sqlx.Stmt `query:"get-exercises"`
 	GetExerciseById *sqlx.Stmt `query:"get-exercise-by-id"`
-	GetWorkouts     *sqlx.Stmt `query:"get-workouts"`
+
+	// Workout queries
+	GetWorkouts *sqlx.Stmt `query:"get-workouts"`
 }
--- queries.sql
+++ queries.sql
@@ -6,3 +6,12 @@ SELECT * FROM exercises;
 
 -- name: get-exercise-by-id
 SELECT * FROM exercises WHERE id = $1;
+
+-- name: get-user-by-id
+SELECT * FROM users WHERE id = $1;
+
+-- name: get-user-by-uuid
+SELECT * FROM users WHERE uid = $1;
+
+-- name: create-user
+INSERT INTO users (uid, email, name, avatar) VALUES ($1, $2, $3, $4) RETURNING *;
